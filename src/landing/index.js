83b27431eb93ad614b5f2fdc1ac01edacad17949
Juan Magarinos <juanchy@gmail.com>
2016-10-04 14:12:20 -0300

    end responsive design para ara sigin/signup. maquetado concluido signin/signup

--- src/landing/index.js
+++ src/landing/index.js
@@ -13,24 +13,27 @@ module.exports = function landing(box) {
       ${box}
     </div>
     <div class="data-box">
-        <div class="box box-1">
-          <div class="icon">
-            <i class="fa fa-certificate fa-3x" aria-hidden="true"></i>
+        <div class="boxing">
+          <div class="box box-1">
+            <div class="icon">
+              <i class="fa fa-certificate fa-3x" aria-hidden="true"></i>
+            </div>
+            <p><strong>Certificados</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam atque nisi eveniet labore sint voluptatum.</p>
           </div>
-          <p><strong>Certificados</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam atque nisi eveniet labore sint voluptatum.</p>
-        </div>
-        <div class="box box-2">
-          <div class="icon">
-            <i class="fa fa-file-text fa-3x iconi" aria-hidden="true"></i>
+          <div class="box box-2">
+            <div class="icon">
+              <i class="fa fa-file-text fa-3x iconi" aria-hidden="true"></i>
+            </div>
+            <p><strong>Examenes</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore assumenda dicta fugit sapiente alias.</p>
           </div>
-          <p><strong>Examenes</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore assumenda dicta fugit sapiente alias.</p>
         </div>
         <div class="box box-3">
           <div class="box-admision">
             <img class="flash-adm" src="flash-adm.png" alt="flash-adm" />
-            <p class="p1-adm">Prueba</p>
-            <p class="p2-adm">tus conocimientos</p>
-            <a class="waves-effect waves-light btn btn-adm">admision</a>
+            <div class="text-admision">
+              <p class="p1-adm">Prueba tus conocimientos</p>
+              <a class="waves-effect waves-light btn btn-adm">admision</a>
+            </div>
           </div>
         </div>
     </div>
